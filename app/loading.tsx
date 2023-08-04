4240ef4242234c4303fffde2542e09d52dc4d03d
DeStRaaM <20destraam03@gmail.com>
2023-08-04 17:22:38 +0200

    Loading animation for routes

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Loader from '../components/common/loader';
+
+export default function Loading() {
+    return (
+        <>
+            <Loader />
+        </>
+    );
+}
--- components/common/loader/index.tsx
+++ components/common/loader/index.tsx
@@ -6,13 +6,15 @@ import { MoonLoader } from 'react-spinners';
 import styles from './styles.module.scss';
 
 export default function Loader() {
-	const loading = /*useSelector((state: RootState) => state.authReducer.isLoading)*/ false;
+    /*const loading = useSelector((state: RootState) => state.authReducer.isLoading);
 
-	if (!loading) return null;
+	if (!loading) return null;*/
 
-	return (
-		<div className={styles.loading}>
-			<MoonLoader color="#36d7b7" loading={loading} />;
-		</div>
-	);
+    const loading = true;
+
+    return (
+        <div className={styles.loading}>
+            <MoonLoader color="#36d7b7" loading={loading} />;
+        </div>
+    );
 }
